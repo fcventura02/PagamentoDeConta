ae65c42b90ace25680693414da8afd639c0a5112
Filipe Ventura <54460658+fcventura02@users.noreply.github.com>
2020-09-13 10:51:09 -0300

    created function calcularTotal

--- app/src/main/java/com/example/pagamentodeconta/MainActivity.kt
+++ app/src/main/java/com/example/pagamentodeconta/MainActivity.kt
@@ -14,16 +14,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var valueRate = 10
         btm_calculate.setOnClickListener {
-            calcular(valueRate)
+            val amount = bill_amount.text.toString()
+            val paid = amount_paid.text.toString()
+            if (amount.isNotEmpty() && paid.isNotEmpty())
+            calcular(valueRate, amount.toFloat(), paid.toFloat())
         }
         btm_clean.setOnClickListener {
             clean()
         }
-        rate.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+        rate.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             @SuppressLint("SetTextI18n")
             override fun onProgressChanged(seekBar: SeekBar, progress: Int, formUser: Boolean) {
                 valueRate = progress
                 tv_rate_value.text = "Valor da taxa é de: $valueRate%"
+                val amount = bill_amount.text.toString()
+
+                if (amount.isNotEmpty()) {
+                    calculaTotal(amount.toFloat(),valueRate)
+                }
+
             }
 
             override fun onStartTrackingTouch(p0: SeekBar?) {
@@ -35,39 +44,39 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
     @SuppressLint("SetTextI18n")
-    fun calcular(rate:Int) {
+    fun calcular(rate: Int, amount: Float, paid:Float) {
         tv_change
         if (bill_amount.length() == 0 || amount_paid.length() == 0)
             return Toast.makeText(
                 this@MainActivity,
                 "Insira valores nos campos", Toast.LENGTH_SHORT
             ).show()
+        //calcula a porcentagem e o total da operação
+        val result = calculaTotal(amount, rate)
 
-        val amount = bill_amount.text.toString().toFloat()
-        val paid = amount_paid.text.toString().toFloat()
-
-        if (amount <= 0 || paid <= 0)
-            return Toast.makeText(
-                this@MainActivity,
-                "Insira valores maiores que 0 nos campos", Toast.LENGTH_SHORT
-            ).show()
-
-        val rateValue = amount * rate / 100
-        val result = amount + rateValue
         val change = paid - result
         var message = "Voltar Troco"
 
         if (change < 0)
             message = "Falta Receber"
 
-        text_rate.text = "R$ ${round(rateValue*100)/100}"
-        text_result.text = "R$ $result"
+
         tv_change.text = message
-        text_change.text = "R$ ${round(change*100)/100}"
+        text_change.text = "R$ ${round(change * 100) / 100}"
 
 
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun calculaTotal (amount: Float, rate: Int): Float {
+        val rateValue = amount*rate/100
+        val result = amount + rateValue
+
+        text_rate.text = "R$ ${round(rateValue * 100) / 100}"
+        text_result.text = "R$ $result"
+
+        return result
     }
 
     @SuppressLint("SetTextI18n")
